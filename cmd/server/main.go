e8fb9d84bec4309d432e6a89fd40ded6806725c8
aftabmuddassir <aftabmuddassir147@gmail.com>
2026-02-14 12:37:11 -0500

    fix: auto migration on start

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -79,10 +79,10 @@ func main() {
 		return
 	}
 
-	// // Run migrations automatically on startup
-	// if err := database.AutoMigrate(db); err != nil {
-	// 	log.Fatalf("Failed to run migrations: %v", err)
-	// }
+	// Run migrations automatically on startup
+	if err := database.AutoMigrate(db); err != nil {
+		log.Fatalf("Failed to run migrations: %v", err)
+	}
 
 	// Seed database if flag is set
 	if *seedFlag {
